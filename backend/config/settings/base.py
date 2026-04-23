ed388b12b5505e084ec640d5026035d6a902157c
Fabio García Sánchez <fabiogarciasanchez@Fabios-MacBook-Pro.local>
2026-04-22 23:17:27 -0500

    Improve backend's admin UI adding company personalization

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = "hersa.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -60,7 +60,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
--- backend/hersa/urls.py
+++ backend/hersa/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+admin.site.site_header = "Eventos Hersa"
+admin.site.site_title = "Eventos Hersa"
+admin.site.index_title = "Panel de administración"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
